d1364a3c2cc0b1d3f825bd5d279730861d6cb640
Uniquehellish <sadmanson@aol.com>
2015-09-22 20:38:34 -0500

    Added
    The files

---
+++ DR.java
@@ -0,0 +1,25 @@
+public class DR {
+
+ public static void main(String[] args) {
+ String name = "Uniquehellish";
+ String jobApply = "applying for Developer, or Staff Manager.";
+ String time = "I have been playing dungeon realms since open beta";
+ String why = "I always had a dream to help develop with minecraft plugins, and I love DR, I'm willing to help out at any cost.";
+ int age = 16;
+ 
+ System.out.println("Hello I am" + name);
+ System.out.println("I am currently" + age + "years old");
+ System.out.println("I'm here because I'm" + jobApply);
+ System.out.println("How long have you been playing DR?" + time);
+ System.out.println("Why would you like to be a developer / Staff Manager?" + why);
+ 
+ //            More about Me
+ /* I would really love to become a staff member.
+  * Maybe not particularly a Developer, maybe staff management, or something!
+  * Yes I know a lot about java, but I relize that theres already 3/4 developers. So please consider me
+  * for another role. I have ran 3 servers in past, one in particular is a OP-Prison owned by me, and friend. We had over 130+ players online and the 
+  * server was called "InfernoPrison" Check it out, it was pretty big! Thank you for your time! You can find me on TS!
+  */
+ }
+
+}
